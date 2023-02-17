6ae8fd72e05d06d33acc8fd8e32381d5535a021a
azamatshsh123@gmail.com <azamatshsh123@gmail.com>
2023-02-17 18:21:35 +0300

    Добавьте файлы проекта.

---
+++ 2oop.cpp
@@ -0,0 +1,52 @@
+﻿#include<iostream>
+#include<conio.h>
+class Rectangle {
+public:
+	Rectangle() {
+		length = 10;
+		width = 20;
+		square = length * width;
+		index = 0;
+		name = "qwerty";
+		std::cout << "Rectangle()" << std::endl;
+	};
+	Rectangle(int length, int width) {
+		this->length = length;
+		this->width = width;
+		square = length * width;
+		index = 0;
+		name = "qwerty";
+		std::cout << "Rectangle(int length,int width)" << std::endl;
+	};
+	Rectangle(const Rectangle& copyrectangle) {
+		length = copyrectangle.length;
+		width = copyrectangle.width;
+		square = length * width;
+		index = 0;
+		name = "qwerty";
+		std::cout << "Rectangle(const Rectangle& copyrectangle)" << std::endl;
+	};
+	~Rectangle() {
+		std::cout << length << std::endl << width << std::endl << square << std::endl;
+		std::cout << index << std::endl << name << std::endl;
+		std::cout << "~Rectangle()" << std::endl;
+	};
+	std::string name;
+protected:
+	int square;
+	int length;
+	int width;
+private:
+	int index;
+};
+int main()
+{
+	{
+		Rectangle a;
+		Rectangle b(20, 10);
+		Rectangle c(a);
+	}
+
+	_getch();
+	return 0;
+}
